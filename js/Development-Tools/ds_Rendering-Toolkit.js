b0b8033e32a902f83e9d643635f7bee3cf9c30a2
savanidhruvin <dhruvinsavani9313@gmail.com>
2024-08-09 18:06:21 +0530

    dhruvin9

--- js/Development-Tools/ds_Rendering-Toolkit.js
+++ js/Development-Tools/ds_Rendering-Toolkit.js
@@ -49,3 +49,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// <!-- *********  What Customers Are Saying ********   -->
+const cardFuc = (a) => {
+    let id = a;
+    let arr = ["ds_industry","ds_vendor","ds_academia"]
+    arr.forEach((ele)=>{
+        document.getElementById(ele).classList.add('d-none')
+    })
+    document.getElementById(arr[id]).classList.remove('d-none')
+
+    let id2 = a;
+
+    let arr2 = ["ds_Indu","ds_Inde","ds_Acade"]
+    arr2.forEach((ele)=>{
+        document.getElementById(ele).classList.remove('ds_cust-active')
+    })
+    document.getElementById(arr2[id2]).classList.add('ds_cust-active')
+
+}
