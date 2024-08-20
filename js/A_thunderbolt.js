91a21f42b15e68cfb7bc06c013841a9c346c6882
Akshayvaghasiya2002 <akshay.kalthiyainfotech@gmail.com>
2024-08-20 19:15:50 +0530

    Akshay20

--- js/A_thunderbolt.js
+++ js/A_thunderbolt.js
@@ -47,3 +47,20 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+//////////////////////// show more script /////////////////////////////
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleBtn = document.getElementById('toggleBtn');
+    const hiddenItems = document.querySelectorAll('.hidden-mobile');
+    let isExpanded = false;
+  
+    toggleBtn.addEventListener('click', function() {
+      isExpanded = !isExpanded;
+      
+      hiddenItems.forEach(item => {
+        item.style.display = isExpanded ? 'block' : 'none';
+      });
+  
+      toggleBtn.textContent = isExpanded ? 'Show Less' : 'Show More';
+    });
+  });
+//////////////////////// show more script /////////////////////////////
