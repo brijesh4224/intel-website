7110b3ab97d9ca995b1a6ca7ae562ea0722e6081
ParthVaghasiyakal-072 <parthvaghasiya.kalthiyainfo@gmail.com>
2024-09-20 18:03:04 +0530

    Parth20-9

--- js/A_learn_about_aipc.js
+++ js/A_learn_about_aipc.js
@@ -19,3 +19,17 @@ function showchoose(x) {
         data1.classList.add('d-none')
     }
 }
+
+function V_dynamic_li(id){
+    let ele = document.querySelectorAll(`.v_dynamic_li`)
+    let img = document.querySelectorAll('.V_imag_div')
+    img.forEach((it)=>{
+        it.classList.add('d-none')
+    })
+    img[id].classList.remove('d-none')
+    ele.forEach((el)=>{
+        el.classList.remove('V_active_li')
+    })
+    ele[id].classList.add('V_active_li')
+
+}
